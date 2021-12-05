eacd82381fc27fc09b8eaaca356d01207511bc71
BONDI JAYANTH <jayanth.bondi@leoforce.com>
2021-12-05 13:42:39 +0530

    Implement different Bank account classes

--- BankAccount.cs
+++ BankAccount.cs
@@ -22,14 +22,23 @@ namespace MyBankAccount
             }
         }
         private static int accountNumberSeed = 1234567890;
+        private readonly decimal minimumBalance;
+
         private List<Transaction> allTransactions = new List<Transaction>();
-        public BankAccount(string name, decimal initialBalance)
+
+        public BankAccount(string name, decimal initialBalance) : this(name, initialBalance, 0) { }
+
+        public BankAccount(string name, decimal initialBalance, decimal minimumBalance)
         {
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
             this.Owner = name;
-            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+            this.minimumBalance = minimumBalance;
+            if (initialBalance > 0)
+                MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
         }
+
+        public virtual void PerformMonthEndTransactions() { }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
@@ -47,12 +56,23 @@ namespace MyBankAccount
             {
                 throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
             }
-            if (Balance - amount < 0)
+            var overdraftTransaction = CheckWithdrawalLimit(Balance - amount < minimumBalance);
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+            if (overdraftTransaction != null)
+                allTransactions.Add(overdraftTransaction);
+        }
+
+        protected virtual Transaction? CheckWithdrawalLimit(bool isOverdrawn)
+        {
+            if (isOverdrawn)
             {
                 throw new InvalidOperationException("Not sufficient funds for this withdrawal");
             }
-            var withdrawal = new Transaction(-amount, date, note);
-            allTransactions.Add(withdrawal);
+            else
+            {
+                return default;
+            }
         }
 
         public string GetAccountHistory()
--- Program.cs
+++ Program.cs
@@ -7,37 +7,46 @@ namespace donet_project
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Jayanth Bondi", 1000);
-            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
-            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
-            Console.WriteLine(account.Balance);
-            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
-            Console.WriteLine(account.Balance);
-            Console.WriteLine(account.GetAccountHistory());
+            // var account = new BankAccount("Jayanth Bondi", 1000);
+            // Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            // account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            // Console.WriteLine(account.Balance);
+            // account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            // Console.WriteLine(account.Balance);
+            // Console.WriteLine(account.GetAccountHistory());
 
             // Test that the initial balances must be positive.
-            BankAccount invalidAccount;
-            try
-            {
-                invalidAccount = new BankAccount("invalid", -55);
-            }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Console.WriteLine("Exception caught creating account with negative balance");
-                Console.WriteLine(e.ToString());
-                return;
-            }
+            // BankAccount invalidAccount;
+            // try
+            // {
+            //     invalidAccount = new BankAccount("invalid", -55);
+            // }
+            // catch (ArgumentOutOfRangeException e)
+            // {
+            //     Console.WriteLine("Exception caught creating account with negative balance");
+            //     Console.WriteLine(e.ToString());
+            //     return;
+            // }
 
             // Test for a negative balance.
-            try
-            {
-                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-            }
-            catch (InvalidOperationException e)
-            {
-                Console.WriteLine("Exception caught trying to overdraw");
-                Console.WriteLine(e.ToString());
-            }
+            // try
+            // {
+            //     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            // }
+            // catch (InvalidOperationException e)
+            // {
+            //     Console.WriteLine("Exception caught trying to overdraw");
+            //     Console.WriteLine(e.ToString());
+            // }
+
+            var lineOfCredit = new LineOfCreditAccount("line of credit", 0, 2000);
+            // How much is too much to borrow?
+            lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
+            lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+            lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
+            lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
+            lineOfCredit.PerformMonthEndTransactions();
+            Console.WriteLine(lineOfCredit.GetAccountHistory());
         }
     }
 }
